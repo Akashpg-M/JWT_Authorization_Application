4e328b408d24b05f73fa1af12d912b41be119e38
akashpg-M <waybooster.pg@gmail.com>
2025-01-26 14:16:20 +0530

    Implemented Auth Functionality

--- frontend_dev1/src/App.jsx
+++ frontend_dev1/src/App.jsx
@@ -1,20 +1,125 @@
-import  {Routes, Route} from 'react-router-dom';
+import { Navigate, Routes, Route } from 'react-router-dom';
+
+import DashboardPage from './pages/DashboardPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+
+import LoadingSpinner from './components/LoadingSpinner';
+
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if(!isAuthenticated){
+    return <Navigate to = '/login' replace />;
+  }
+
+  if(!user.isVerified){
+    return <Navigate to='/verify-email' replace />;
+  }
+
+  return children;
+}
+
+//redirect Authenticated user
+const RedirectAuthenticatedUser = ({children}) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if(isAuthenticated && user.isVerified){
+    return <Navigate to='/' replace />;
+  }
+
+  return children; // children = current page
+}
+
 
 function App() {
   console.log("App loaded");
+
+  const {isCheckingAuth, checkAuth , isAuthenticated, user} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth]);
+  
+  if(isCheckingAuth) return <LoadingSpinner />
+
+
+  console.log("IsAuthenticated: ", isAuthenticated);
+  console.log("user; ", user);
+
   return (
     <>
     <div>
       <h1 className = 'text-red-500 text-5xl'> Hello World </h1>
       <Routes>
-        <Route path='/' element = {"Home"} />
-        <Route path = '/signup' element = {<SignUpPage/>}/>
-        <Route path = '/login' element = {<LoginPage/>} />
-        <Route path = '/verify-email' element= {<EmailVerificationPage />} />
+
+        <Route 
+          path='/' 
+          element = {
+            <ProtectedRoute>
+              <DashboardPage/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route 
+          path = '/signup' 
+          element = {
+          <RedirectAuthenticatedUser>
+            <SignUpPage/>
+          </RedirectAuthenticatedUser>
+          }
+        />
+
+        <Route 
+          path = '/login' 
+          element = {
+          <RedirectAuthenticatedUser>
+            <LoginPage/>
+          </RedirectAuthenticatedUser>
+          } 
+        />
+
+        <Route  
+          path = '/verify-email' 
+          element= {
+              <EmailVerificationPage />
+          } 
+        />
+
+        <Route
+          path = '/forgot-password'
+          element={
+            <RedirectAuthenticatedUser>
+              <ForgotPasswordPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+
+        <Route
+          path='reset-password/:token'
+          element={
+            <RedirectAuthenticatedUser>
+              <ResetPasswordPage/>
+            </RedirectAuthenticatedUser>
+          }
+        />
+
+        <Route 
+          path='*' 
+          element={<Navigate to='/' replace />} 
+        />
+
       </Routes>
+      <Toaster/>
     </div>
       
     </>
@@ -23,10 +128,89 @@ function App() {
 
 export default App
 
-// import React from "react";
+
+// import { Navigate, Route, Routes } from "react-router-dom";
+
+// import SignUpPage from "./pages/SignUpPage";
+// import LoginPage from "./pages/LoginPage";
+// import EmailVerificationPage from "./pages/EmailVerificationPage";
+// import DashboardPage from "./pages/DashboardPage";
+
+// import LoadingSpinner from "./components/LoadingSpinner";
+
+// import { Toaster } from "react-hot-toast";
+// import { useAuthStore } from "./store/authStore";
+// import { useEffect } from "react";
+
+// // protect routes that require authentication
+// const ProtectedRoute = ({ children }) => {
+// 	const { isAuthenticated, user } = useAuthStore();
+
+// 	if (!isAuthenticated) {
+// 		return <Navigate to='/login' replace />;
+// 	}
+
+// 	if (!user.isVerified) {
+// 		return <Navigate to='/verify-email' replace />;
+// 	}
+
+// 	return children;
+// };
+
+// // redirect authenticated users to the home page
+// const RedirectAuthenticatedUser = ({ children }) => {
+// 	const { isAuthenticated, user } = useAuthStore();
+
+// 	if (isAuthenticated && user.isVerified) {
+// 		return <Navigate to='/' replace />;
+// 	}
+
+// 	return children;
+// };
 
 // function App() {
-//   return <h1>Hello World</h1>;
+// 	const { isCheckingAuth, checkAuth } = useAuthStore();
+
+// 	useEffect(() => {
+// 		checkAuth();
+// 	}, [checkAuth]);
+
+// 	if (isCheckingAuth) return <LoadingSpinner />;
+
+// 	return (
+// 		<div>
+			
+// 			<Routes>
+// 				<Route
+// 					path='/'
+// 					element={
+// 						<ProtectedRoute>
+// 							<DashboardPage />
+// 						</ProtectedRoute>
+// 					}
+// 				/>
+// 				<Route
+// 					path='/signup'
+// 					element={
+// 						<RedirectAuthenticatedUser>
+// 							<SignUpPage />
+// 						</RedirectAuthenticatedUser>
+// 					}
+// 				/>
+// 				<Route
+// 					path='/login'
+// 					element={
+// 						<RedirectAuthenticatedUser>
+// 							<LoginPage />
+// 						</RedirectAuthenticatedUser>
+// 					}
+// 				/>
+// 				<Route path='/verify-email' element={<EmailVerificationPage />} />
+				
+// 			</Routes>
+// 			<Toaster />
+// 		</div>
+// 	);
 // }
 
 // export default App;
---
+++ frontend_dev1/src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useAuthStore } from "../store/authStore";
+import Input from "../components/Input";
+import { ArrowLeft, Loader, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const ForgotPasswordPage = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const { isLoading, forgotPassword } = useAuthStore();
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    await forgotPassword(email);
+    setIsSubmitted(true);
+  };
+
+  return(
+    <motion.div
+      initial = {{ opacity:0, y: 20}}
+      animate ={{opacity: 1, y: 0}}
+      transition = {{ duration: 0.5}}
+      >
+        <div classname='p-8'>
+          <h2>
+            Forgot Password
+          </h2>
+
+          {!isSubmitted ? (
+            <form onSubmit={handelSubmit}>
+              <p classname='text-grey-300 mb-6 text-center'>
+                Enter your emial and we'll send you a link to reset your password.
+              </p>
+
+              <Input
+                icon={Mail}
+                type='email'
+                placeholder='Email Address'
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+              />
+
+              <motion.button>
+                {isLoading ? <Loader classname='size-6 animate-spin mx-auto' /> : "send Reset Link"}
+              </motion.button>
+            </form>
+          ) : (
+            <div className='text-center'>
+              <motion.div>
+                <Mail className='h-8 w-8 text-white'/>
+              </motion.div>
+
+              <p className='text-grey-300 mb-6'>
+                If an account exits from {email}, you will receive a password reset link shortly.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+          <Link to={"/login"}>
+            <ArrowLeft className='h-4 w-4 mr-2'/> Back to Login
+          </Link>
+        </div>
+      </motion.div>
+  )
+}
+
+export default ForgotPasswordPage;
