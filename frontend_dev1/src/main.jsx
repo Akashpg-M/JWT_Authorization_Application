244f0a0c240ce612aa507a3d9ffd097dad8e1004
akashpg-M <waybooster.pg@gmail.com>
2025-02-01 17:40:02 +0530

    Moved files to frontend folder from frontend_dev1

--- frontend_dev1/src/main.jsx
+++ frontend_dev1/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
+import './Auth_Application/index.css'
 import App from './Auth_Application/App.jsx'
 
 createRoot(document.getElementById('root')).render(
